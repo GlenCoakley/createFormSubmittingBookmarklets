6e779b6b54eb4aa50f0daec11898e208378de415
Glen <GlenCoakley@users.noreply.github.com>
2018-10-27 08:10:53 -0500

    Linted

--- src/js/createFormSubmitBookmarklets.js
+++ src/js/createFormSubmitBookmarklets.js
@@ -1,43 +1,46 @@
-function buildBookmark(form) {  
+[200~function buildBookmark(form) {
   function getFormParams(form) {
     function addParam(name, value, out) {  return out + (out ? ',' : '') + '"' + name + '":"' + value + '"' };
-    function addAll(form, tag, out) { 
-      for (const els of form.getElementsByTagName(tag)) {  out = addParam (els.name, els.value, out); } 
-      return out 
+    function addAll(form, tag, out) {
+      for (var els of form.getElementsByTagName(tag)) { 
+        out = addParam (els.name, els.value, out);
+      }
+      return out
     }
     var out = ''
-    for (const tag of ['input', 'textarea', 'submit']) { 
-      out = addAll(form, tag, out); 
+    for (var tag of ['input', 'textarea', 'submit']) {
+      out = addAll(form, tag, out);
     }
-    var params = ''; 
-    for (const sels of form.getElementsByTagName('select')) {
-      for (const opts of form.getElementsByTagName('option')) {
+    var params = '';
+    for (var sels of form.getElementsByTagName('select')) {
+      for (var opts of form.getElementsByTagName('option')) {
           if (opts.selected) { out = addParam (sels.name, opts.value, out); }
       };
     };
     return '{' + out + '}';
   };
   var header = '<h4>Form - ';
-  if (form.name) header += ' name: ' + form.name; 
+  if (form.name) header += ' name: ' + form.name;
   if (form.id) header += ', id: ' + form.id;
   header += ', action: ' + form.action + '</h4>';
-  return header + 'javascript:function sf(ur,ty,fd){' + 
-    'function me(tg,pr){var el=document.createElement(tg);' + 
-    'for(const[nm,vl]of Object.entries(pr)){el.setAttribute(nm,vl);}return el}' + 
-    'const fm=me("form",{action:ur,method:ty,style:"display:none"});' + 
+  return header + 'javascript:function sf(ur,ty,fd){' +
+    'function me(tg,pr){var el=document.createElement(tg);' +
+    'for(const[nm,vl]of Object.entries(pr)){el.setAttribute(nm,vl);}return el}' +
+    'const fm=me("form",{action:ur,method:ty,style:"display:hidden;"});' +
     'for(const[nm,vl]of Object.entries(fd)){' +
-    'fm.appendChild(me("input",{name:nm, value:vl}))' + 
-    '}document.body.appendChild(fm);fm.submit()' + 
-    '}sf("' + form.action + '","' + form.method + '",' + getFormParams(form) + ');'; 
+    'fm.appendChild(me("input",{name:nm, value:vl}))' +
+    '}document.body.appendChild(fm);fm.submit()' +
+    '}sf("' + form.action + '","' + form.method + '",' + getFormParams(form) + ');';
 };
 try {
   var bookmarks = '';
-  for (const form of window.document.getElementsByTagName('form')) { 
-    bookmarks += buildBookmark(form) + '\r\n'; 
+  for (var form of window.document.getElementsByTagName('form')) {
+    bookmarks += buildBookmark(form) + '\r\n';
   }
 } catch (e) { console.trace(e);alert(e) }
-var handle = open('', 'bbout', "width=800,height=600,resizable,scrollbars");
-handle.document.write('<html><title>Form Bookmarklets</title><body>' + bookmarks + '</body></html>');
+var winHndl  = open('', 'bbout', "width=800,height=600,resizable,scrollbars");
+winHndl.document.write('<html><title>Form Bookmarklets</title><body>' + 
+  bookmarks + '</body></html>');
 
 
 // Use something that understands ES6 to make it into a bookmarklet.
